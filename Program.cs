1dc58057c5d31c7196d63fc591e9e5540357c514
nyankittone <tiffany.farris@proton.me>
2026-02-22 07:13:27 -0500

    Basic tokenizer

--- Program.cs
+++ Program.cs
@@ -1,13 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections.Generic;
+using System.Linq;
 // using System.Text;
+using RE = System.Text.RegularExpressions;
 namespace Calculator;
 
 class Program {
     private static IEnumerable<string> Tokenize(string input) {
-        foreach(string bigToken in input.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)) {
-            yield return bigToken;
+        RE.Regex re = new(@"[\+\-\*\/]");
+
+        foreach(string bigToken in String.Concat(input.Select((thing) => thing == '\t' ? ' ' : thing))
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries))
+        {
+            var matches = re.Matches(bigToken);
+            int numberBegin = 0;
+
+            foreach(RE.Match match in matches) {
+                // get token to the left of the operator
+                int numberEnd = match.Index;
+                string number = bigToken[numberBegin..numberEnd];
+                if(number.Length > 0) {
+                    yield return number;
+                }
+
+                // get operator
+                switch(match.Value) {
+                    case "+":
+                        yield return "+";
+                        break;
+                    case "-":
+                        yield return "-";
+                        break;
+                    case "*":
+                        yield return "*";
+                        break;
+                    case "/":
+                        yield return "/";
+                        break;
+                    default:
+                        throw new NotImplementedException("wtf if this operator bruh");
+                }
+
+                numberBegin = numberEnd + match.Length;
+            }
+
+            string finalNumber = bigToken[numberBegin..];
+            if(finalNumber.Length > 0) {
+                yield return finalNumber;
+            }
         }
     }
 
@@ -17,7 +58,7 @@ class Program {
         // predefined functions to compute
         // order of operations
         // reasonable errors
-        
+
         // Create a tokenizer first
         return 42;
     }
@@ -28,6 +69,8 @@ class Program {
             foreach(string token in Tokenize(line)) {
                 Console.WriteLine($"\"{token}\"");
             }
+
+            Console.Error.Write("> ");
         }
     }
 }
