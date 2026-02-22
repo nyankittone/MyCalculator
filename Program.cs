7e3988e62de38fed0198b9267ab2dbb17afc178b
nyankittone <tiffany.farris@proton.me>
2026-02-22 08:36:47 -0500

    Wrote some nodes

--- Program.cs
+++ Program.cs
@@ -6,6 +6,88 @@ using System.Linq;
 using RE = System.Text.RegularExpressions;
 namespace Calculator;
 
+enum LexemeID {
+    Number,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Func,
+}
+
+// We're going to make the parser also take the role of the lexer, for convenience on my end bc I
+// don't feel like being smart right now.
+interface IExpression {
+    public LexemeID ID {get;}
+    public decimal Evaluate();
+}
+
+class Number : IExpression {
+    private decimal number;
+    public LexemeID ID {get;} = LexemeID.Number;
+
+    public Number(string token) {
+        number = Decimal.Parse(token); // TODO: Think about error handling here.
+    }
+
+    public decimal Evaluate() => number;
+}
+
+abstract class Operator : IExpression {
+    public abstract LexemeID ID {get;}
+    protected IExpression left;
+    protected IExpression right;
+
+    public Operator(IExpression left, IExpression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public abstract decimal Evaluate();
+}
+
+// I reeeeeally wish I didn't have to explicitly mention the constructor in every derived class.
+// That's a little annoying.
+class Add : Operator {
+    public override LexemeID ID {get;} = LexemeID.Add;
+    public Add(IExpression left, IExpression right) : base(left, right) {}
+
+    public override decimal Evaluate() => left.Evaluate() + right.Evaluate();
+}
+
+class Subtract : Operator {
+    public override LexemeID ID {get;} = LexemeID.Subtract;
+    public Subtract(IExpression left, IExpression right) : base(left, right) {}
+
+    public override decimal Evaluate() => left.Evaluate() - right.Evaluate();
+}
+
+class Multiply : Operator {
+    public override LexemeID ID {get;} = LexemeID.Multiply;
+    public Multiply(IExpression left, IExpression right) : base(left, right) {}
+
+    public override decimal Evaluate() => left.Evaluate() * right.Evaluate();
+}
+
+class Divide : Operator {
+    public override LexemeID ID {get;} = LexemeID.Divide;
+    public Divide(IExpression left, IExpression right) : base(left, right) {}
+
+    public override decimal Evaluate() => left.Evaluate() / right.Evaluate();
+}
+
+class Sqrt : IExpression {
+    public LexemeID ID {get;} = LexemeID.Func;
+    private IExpression unsquared;
+
+    public Sqrt(IExpression expr) {
+        unsquared = expr;
+    }
+
+    // grrrrrr I hate these casts
+    public decimal Evaluate() => (decimal)Math.Sqrt((double)unsquared.Evaluate());
+}
+
 class Program {
     private static IEnumerable<string> Tokenize(string input) {
         RE.Regex re = new(@"[\+\-\*\/]");
@@ -39,7 +121,7 @@ class Program {
                         yield return "/";
                         break;
                     default:
-                        throw new NotImplementedException("wtf if this operator bruh");
+                        throw new NotImplementedException("wtf is this operator bruh");
                 }
 
                 numberBegin = numberEnd + match.Length;
