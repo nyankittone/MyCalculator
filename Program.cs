1b888ac99a7b97136dcf4381401ecda3a3fbfc5e
nyankittone <tiffany.farris@proton.me>
2026-02-22 06:42:20 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+// using System.Text;
+namespace Calculator;
+
+class Program {
+    private static IEnumerable<string> Tokenize(string input) {
+        foreach(string bigToken in input.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)) {
+            yield return bigToken;
+        }
+    }
+
+    private static decimal Resolve(string input) {
+        // My expression resolver should support:
+        // addition, subtraction, multiplication, division, and exponents
+        // predefined functions to compute
+        // order of operations
+        // reasonable errors
+        
+        // Create a tokenizer first
+        return 42;
+    }
+
+    static void Main(string[] args) {
+        Console.Error.Write("> ");
+        while(Console.ReadLine() is string line) {
+            foreach(string token in Tokenize(line)) {
+                Console.WriteLine($"\"{token}\"");
+            }
+        }
+    }
+}
+
