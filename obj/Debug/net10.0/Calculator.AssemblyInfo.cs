4cf8eb5fb03c2ed2d8f5b53db0c97d40ba8342d3
nyankittone <tiffany.farris@proton.me>
2026-02-27 05:21:06 -0500

    a

--- obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63bd89c18b31dc2b5a0a92364cbc56de5194375e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e7e90347873fa51ea51c6bfc10e2e7ec298383")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
