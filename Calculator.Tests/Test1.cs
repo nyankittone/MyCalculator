d685edda6e3ede24e3b15a5d740ac624e15eac26
nyankittone <tiffany.farris@proton.me>
2026-02-28 20:44:14 -0500

    Write some unit tests

--- Calculator.Tests/Test1.cs
+++ Calculator.Tests/Test1.cs
@@ -1,11 +1,123 @@
 ﻿namespace Calculator.Tests;
 
 [TestClass]
-public sealed class Test1
+public sealed class LexerTests
 {
+    private Lexeme Add = Lexeme.Operator("+");
+    private Lexeme Sub = Lexeme.Operator("-");
+    private Lexeme Mult = Lexeme.Operator("*");
+    private Lexeme Div = Lexeme.Operator("/");
+    private Lexeme Exp = Lexeme.Operator("**");
+
+    private void DidItTwoPlusTwo(Lexeme[] testOn) {
+        Assert.HasCount(3, testOn);
+        Assert.AreEqual(testOn[0], Lexeme.Number("2"));
+        Assert.AreEqual(testOn[1], Add);
+        Assert.AreEqual(testOn[2], Lexeme.Number("2"));
+    }
+
     [TestMethod]
     public void TwoPlusTwo()
     {
-        Assert.AreEqual(2, 2);
+        var result = Lexer.Lex("2 + 2").ToArray();
+        DidItTwoPlusTwo(result);
+    }
+
+    [TestMethod]
+    public void TwoPlusTwoCompressed() {
+        var result = Lexer.Lex("2+2").ToArray();
+        DidItTwoPlusTwo(result);
+    }
+
+    [TestMethod]
+    public void TwoPlusTwoWhitespace() {
+        var result = Lexer.Lex("    2     + 2       ").ToArray();
+        DidItTwoPlusTwo(result);
+    }
+
+    [TestMethod]
+    public void TwoPlusTwoTabs() {
+        var result = Lexer.Lex("\t2\t+\t2\t").ToArray();
+        DidItTwoPlusTwo(result);
+    }
+
+    [TestMethod]
+    public void TwoPlusPositiveTwo() {
+        var result = Lexer.Lex("2++2").ToArray();
+        Assert.HasCount(3, result);
+        Assert.AreEqual(result[0], Lexeme.Number("2"));
+        Assert.AreEqual(result[1], Add);
+        Assert.AreEqual(result[2], Lexeme.Number("+2"));
+    }
+
+    [TestMethod]
+    public void BrokenTwoPlusPositiveTwo() {
+        var result = Lexer.Lex("2 ++ 2").ToArray();
+        Assert.HasCount(4, result);
+        Assert.AreEqual(result[0], Lexeme.Number("2"));
+        Assert.AreEqual(result[1], Add);
+        Assert.AreEqual(result[2], Add);
+        Assert.AreEqual(result[3], Lexeme.Number("2"));
+    }
+
+    [TestMethod]
+    public void SubtractThing() {
+        var result = Lexer.Lex("69-420").ToArray();
+        Assert.HasCount(3, result);
+        Assert.AreEqual(result[0], Lexeme.Number("69"));
+        Assert.AreEqual(result[1], Sub);
+        Assert.AreEqual(result[2], Lexeme.Number("420"));
+    }
+
+    [TestMethod]
+    public void DoubleSubtractThing() {
+        var result = Lexer.Lex("69--420").ToArray();
+        Assert.HasCount(3, result);
+        Assert.AreEqual(result[0], Lexeme.Number("69"));
+        Assert.AreEqual(result[1], Sub);
+        Assert.AreEqual(result[2], Lexeme.Number("-420"));
+    }
+
+    [TestMethod]
+    public void BrokenDoubleSubtractThing() {
+        var result = Lexer.Lex("69 -- 420").ToArray();
+        Assert.HasCount(4, result);
+        Assert.AreEqual(result[0], Lexeme.Number("69"));
+        Assert.AreEqual(result[1], Sub);
+        Assert.AreEqual(result[2], Sub);
+        Assert.AreEqual(result[3], Lexeme.Number("420"));
+    }
+
+    [TestMethod]
+    public void OperatorSpam() {
+        var result = Lexer.Lex("+*-///---+-+-/*+**-/+").ToArray();
+        Lexeme[] expected = {
+            Add,
+            Mult,
+            Sub,
+            Div,
+            Div,
+            Div,
+            Sub,
+            Sub,
+            Sub,
+            Add,
+            Sub,
+            Add,
+            Sub,
+            Div,
+            Mult,
+            Add,
+            Exp,
+            Sub,
+            Div,
+            Add,
+        };
+
+        Assert.HasCount(expected.Length, result);
+        // can I just Assert.AreEqual() the entire array at once?
+        for(int i = 0; i < result.Length; i++) {
+            Assert.AreEqual(result[i], expected[i]);
+        }
     }
 }
--- Calculator.Tests/obj/Debug/net10.0/Calculator.Tests.AssemblyInfo.cs
+++ Calculator.Tests/obj/Debug/net10.0/Calculator.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd08fc41b4c0fb17e62739a0a504af6699178d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e457d5c353e436735efe35130444f47b88d31d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
